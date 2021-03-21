692bd625797f5098a56978094d647d13aa14bb42
huga55 <xp_92@mail.ru>
2021-03-22 00:09:50 +0500

    fix

--- src/API/API.ts
+++ src/API/API.ts
@@ -2,11 +2,14 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: "https://wimdev.ru/movie",//"http://localhost:8080/",
+    headers: {
+        "Content-Type" : "application/json",
+    },
 });
 
 export const articlesAPI = {
     getArticles() {
-        return instance.get("").then(response => response);
+        return instance.get("/").then(response => response);
     }
 }
